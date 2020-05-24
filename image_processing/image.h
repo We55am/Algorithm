086c0f39768d305b53e75ecbe9bb8582cb0bb268
We55am <wessam.shoman@gmail.com>
2020-05-25 00:17:04 +0200

    editing headers

--- image_processing/image.h
+++ image_processing/image.h
@@ -1,13 +1,12 @@
 #ifndef __IMAGE_H_
 #define __IMAGE_H_
 
-
 typedef struct _image{
     int row;
     int col;
     int *data;
 } Image;
 
-
+#define _matrix(kernal, x, y) *(*(kernal+x)+y)
 
 #endif // __IMAGE_H_
