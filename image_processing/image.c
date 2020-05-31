cffdcc946c9b1f402c91e1abed1ef20a4c038dbb
We55am <wessam.shoman@gmail.com>
2020-05-31 11:43:55 +0200

    edit img

--- image_processing/image.c
+++ image_processing/image.c
@@ -1,25 +1,5 @@
 #include "image.h"
 
-static int matrix_sum_int(float matrix[SIZE][SIZE], int size)
-{
-    int result = 0;
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            result += matrix[i][j];
-    return result;
-}
-
-static float matrix_sum_float(float matrix[SIZE][SIZE], int size)
-{
-    double result = 0;
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            result += matrix[i][j];
-    return result;
-}
-
-
-
 int main()
 {
     
@@ -50,6 +30,16 @@ int main()
     return EXIT_SUCCESS;
 }
 
+int image_init(Image *img, int raw, int col, void (*destroy)(void *data))
+{
+    img_row(img) = raw;
+    img_col(img) = col;
+    img->destroy = destroy;
+    if((img->data = (int *)malloc(raw*col*sizeof(int))) == NULL)
+        return -1;
+    return 1;
+}
+
 void print_kernal(float kernal[SIZE][SIZE])
 {
     printf("\nKernal .... :\n");
@@ -62,3 +52,22 @@ void print_kernal(float kernal[SIZE][SIZE])
         printf("\n");
     }
 }
+
+int matrix_sum_int(int matrix[SIZE][SIZE], int size)
+{
+    int result = 0;
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            result += matrix[i][j];
+    return result;
+}
+
+float matrix_sum_float(float matrix[SIZE][SIZE], int size)
+{
+    double result = 0;
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            result += matrix[i][j];
+    return result;
+}
+
--- image_processing/image.h
+++ image_processing/image.h
@@ -15,10 +15,20 @@ typedef struct _image{
     int row;
     int col;
     int *data;
+    void (*destroy)(void *data);
 } Image;
 
+
+void image_init(Image *img, int raw, int col, void (*destroy)(void *data));
+void image_destory(Image *img);
 void print_kernal(float kernal[SIZE][SIZE]);
+float matrix_sum_float(float matrix[SIZE][SIZE], int size);
+int matrix_sum_int(int matrix[SIZE][SIZE], int size);
 
+void corss_correlation(int kernal[SIZE][SIZE], Image *img);
+
+#define img_pixel(img, y, x) ()
 #define _matrix(kernal, x, y) *(*(kernal+x)+y)
-
+#define img_row(img) (img->row)
+#define img_col(img) (img->col)
 #endif // __IMAGE_H_
